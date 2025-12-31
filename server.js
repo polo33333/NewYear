4b2bf9e8268fa27c0d23984b8cbb875bc203cf5b
Trần Đức Thái <tranducthai8@gmail.com>
2026-01-01 04:26:14 +0700

    Use environment variable for server port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 4200;
+const PORT = process.env.PORT || 4200;
 
 app.use(cors());
 app.use(bodyParser.json());
